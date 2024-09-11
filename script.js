6609d55c320fb953ed6894ec672782793a5cccfc
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-09-11 15:33:27 +0100

    Add comments to each function, create a variable that listens for clicks on the calculator, stores the value in a variable and also displays the value

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+// Create a function to add two values.
 const add = function(firstNumber,lastNumber){
     let operator = '+';
     return (firstNumber + lastNumber)
 }
-
+// Create a function to subtract two values.
 const subtract = function(firstNumber,lastNumber){
     let operator = '-';
     return (firstNumber-lastNumber)
 }
 
+// Create a function to multiply two values.
 const multiply = function(firstNumber,lastNumber){
     let operator = '*';
     return (firstNumber*lastNumber)
 }
 
+// Create a function to divide two values.
 const divide = function(firstNumber,lastNumber){
     let operator = '/';
     return (firstNumber/lastNumber)
 }
 
+// Create a function to operate that calls on the previously declared functions depending on the operator.
 const operate = (firstNumber, lastNumber, operator) => {
     if(operator = '+'){
         const result = add(firstNumber,lastNumber);
@@ -40,13 +44,14 @@ const operate = (firstNumber, lastNumber, operator) => {
 
 }
 
-
-// let firstNumber = a;
-// let lastNumber = b;
-// let operators = {
-//     '+': add,
-//     '-': subtract,
-//     '*': multiply,
-//     '/': divide,
-// }
+/*Create a listening variable, that identifies what has been clicked on the calculator
+extracts it's html value and then displays's it on the display secction. */
+const buttonClick = document.getElementById("container");
+buttonClick.addEventListener("click", function(event){
+    const buttonContent = event.target.value;
+    // Select the div by its ID and set it as it's innerHTML to display.
+    document.getElementById("display").innerHTML = buttonContent;
+    let firstButton = buttonContent;
+    console.log(firstButton);
+});
 
